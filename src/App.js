49cb06240987c66b4c58be596d959cdf2215f5fb
Robbie DeHaan <robbiedehaan@gmail.com>
2024-05-02 16:14:34 -0400

    added logo and component slots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import React from 'react';
+import { Amplify } from 'aws-amplify';
 
-function App( { signOut, user } ) {
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Text, View, Image, useTheme } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="Amplify logo"
+          src="https://github.com/robbliebob/amplofi/blob/main/logo.png?raw=true"
+        />
+      </View>
+    );
+  },
+};
+
+export default function App() {
   return (
-    <div className="App">
-      {user.attributes.email}
-      <button onClick={signOut}>Sign out</button>
+    <div>
+      <components.Header />
+      <Authenticator socialProviders={['google', 'amazon']}>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
     </div>
   );
 }
-
-export default withAuthenticator(App, {
-  socialProviders: ['google', 'amazon']
-});
